6c1ec73c64bf854f2f7f8f3f6790ef6049b8a7b9
poroto82 <egadea@aper.com>
2024-02-02 15:02:17 -0300

    Auth Controller, passport and routes

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,57 @@
+<?php
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+use App\Models\User;
+
+class AuthController extends Controller
+{
+    // Registro de un nuevo usuario
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|unique:users|max:255',
+            'password' => 'required|string|min:8',
+        ]);
+
+        if ($validator->fails()) {
+            return response(['errors' => $validator->errors()->all()], 422);
+        }
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+        ]);
+
+        $token = $user->createToken('MyApp')->accessToken;
+
+        return response(['token' => $token], 200);
+    }
+
+    // Inicio de sesión
+    public function login(Request $request)
+    {
+        $credentials = request(['email', 'password']);
+
+        if (!Auth::attempt($credentials)) {
+            return response(['error' => 'Invalid credentials'], 401);
+        }
+
+        $user = $request->user();
+        $token = $user->createToken('MyApp')->accessToken;
+
+        return response(['token' => $token], 200);
+    }
+
+    // Cierre de sesión
+    public function logout(Request $request)
+    {
+        $request->user()->token()->revoke();
+
+        return response(['message' => 'Successfully logged out'], 200);
+    }
+}
---
+++ app/Repositories/ZenQuoteRepository.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Repositories;
+
+use Illuminate\Support\Facades\Http;
+
+class ZenQuoteRepository{
+
+    public function getRandomQuote(){
+         // Realizar la solicitud a la API de Quotes
+         $response = Http::get('https://zenquotes.io/api/random');
+
+         // Verificar si la solicitud fue exitosa y devolver los datos
+         if ($response->successful()) {
+             return $response->json();
+         }
+ 
+         // Manejar cualquier error o fallback
+         return null;
+    }
+}
